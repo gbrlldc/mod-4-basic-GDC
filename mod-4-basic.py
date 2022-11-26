6a2274a4b3b9de62e68b1adf1bf1ecb73dde69d1
Gab DC <gabrielle.delacruz@obf.ateneo.edu>
2022-11-27 02:33:34 +0800

    re-commit

--- mod-4-basic.py
+++ mod-4-basic.py
@@ -3,7 +3,7 @@ Useful Business Calculations
 This assignment covers your basic proficiency with Python.
 '''
 
-def savings(grosspay, taxrate, expenses):
+def savings(gross_pay, tax_rate, expenses):
     '''Savings.
     2 points.
     This function calculates the money remaining
